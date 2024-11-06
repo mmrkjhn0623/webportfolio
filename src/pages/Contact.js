5b19a2b11e20a73c9dc84ffd51cc66d309f3d459
mmrkjhn <mmrkjhn@gmail.com>
2024-11-06 10:29:08 +0800

    Added Mailing Service

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,13 @@
-import {onPageLoad, ContentHeader} from "./CustomFunction.js";
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal';
-import Axios from 'axios';
+import { onPageLoad, ContentHeader } from "./CustomFunction.js";
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import Axios from "axios";
+import emailjs from "@emailjs/browser";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 const Contact = () => {
-    //const themecolor = localStorage.getItem("themecolor");
+  //const themecolor = localStorage.getItem("themecolor");
 
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
@@ -17,65 +18,100 @@ const Contact = () => {
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
+    subtitle.style.color = "#f00";
   }
 
   function closeModal() {
     setIsOpen(false);
   }
 
-    useEffect(() => {
-        onPageLoad();
-        ContentHeader("Contact Me","contact");
-       // document.getElementById("overlay").style.backgroundColor = "rgba("+themecolor+",0.60)";
-    },[]);
+  useEffect(() => {
+    onPageLoad();
+    ContentHeader("Contact Me", "contact");
+    // document.getElementById("overlay").style.backgroundColor = "rgba("+themecolor+",0.60)";
+  }, []);
 
-    const SendMessage = (e) =>{
-        e.preventDefault();
-        document.getElementById('messagesend').value = "Sending...";
-        document.getElementById('messagesend').disabled = true;
-        Axios.post('newmessage',{
-            'name':  document.getElementById('name').value,
-            'email': document.getElementById('email').value,
-            'message': document.getElementById('message').value
-        }).then(()=>{
-            setTimeout(() => {
-                document.getElementById('messagesend').value = "Send!"; 
-                document.getElementById('messagesend').disabled = false;
-                openModal();    
-            },2000);
-        });
-    }
+  const SendMessage = (e) => {
+    e.preventDefault();
+    document.getElementById("messagesend").value = "Sending...";
+    document.getElementById("messagesend").disabled = true;
+    // Axios.post("newmessage", {
+    //   name: document.getElementById("name").value,
+    //   email: document.getElementById("email").value,
+    //   message: document.getElementById("message").value,
+    // }).then(() => {
+    //   setTimeout(() => {
+    //     document.getElementById("messagesend").value = "Send!";
+    //     document.getElementById("messagesend").disabled = false;
+    //     openModal();
+    //   }, 2000);
+    // });
+    emailjs
+      .send(
+        "service_ar0ifgb",
+        "template_tgio32o",
+        {
+          from_name: document.getElementById("name").value,
+          to_name: "MJ",
+          message: document.getElementById("message").value,
+          reply_to: document.getElementById("email").value,
+        },
+        {
+          publicKey: "lR9LmmL0XNeKWf5V5",
+        }
+      )
+      .then(() => {
+        setTimeout(() => {
+          document.getElementById("messagesend").value = "Send!";
+          document.getElementById("messagesend").disabled = false;
+          openModal();
+        }, 2000);
+      });
+  };
 
-    return (<>
-        <div className="container" style={{maxWidth: "680px", marginBottom: "120px"}}>
-            <div className="cont-header">
-                <span className="cont-label"><b>Let's work together!</b></span>
-                <p>Like my work and want something similar for your company? Sure, let's get to business.</p>
-            </div>
-            <form className="contactform" onSubmit={SendMessage}>
-                <div className="form-group">
-                <input type="text" id="name" placeholder="Your Name" required/>
-                <input type="email" id="email" placeholder="Your Email" required/>
-                </div>
-                <textarea placeholder="Your Message" id="message" required></textarea>
-                <input type="submit" value="Send!" id="messagesend" />
-            </form>
-        </div>
-      <div>
-      <Modal className="modal" /*style={{backgroundColor: "rgba("+themecolor+",0.60)"}}*/
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        contentLabel="Example Modal"
+  return (
+    <>
+      <div
+        className="container"
+        style={{ maxWidth: "680px", marginBottom: "120px" }}
       >
-        <h2>Thank you for sending message, I'll reach out with you as soon as possible.  </h2>
-        <a href="#" className="closemodal" onClick={closeModal}>x</a>
-      </Modal>
-    </div>
-    </>)
-
+        <div className="cont-header">
+          <span className="cont-label">
+            <b>Let's work together!</b>
+          </span>
+          <p>
+            Like my work and want something similar for your company? Sure,
+            let's get to business.
+          </p>
+        </div>
+        <form className="contactform" onSubmit={SendMessage}>
+          <div className="form-group">
+            <input type="text" id="name" placeholder="Your Name" required />
+            <input type="email" id="email" placeholder="Your Email" required />
+          </div>
+          <textarea placeholder="Your Message" id="message" required></textarea>
+          <input type="submit" value="Send!" id="messagesend" />
+        </form>
+      </div>
+      <div>
+        <Modal
+          className="modal" /*style={{backgroundColor: "rgba("+themecolor+",0.60)"}}*/
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+        >
+          <h2>
+            Thank you for sending message, I'll reach out with you as soon as
+            possible.{" "}
+          </h2>
+          <a href="#" className="closemodal" onClick={closeModal}>
+            x
+          </a>
+        </Modal>
+      </div>
+    </>
+  );
 };
-
 
 export default Contact;
--- src/pages/CustomFunction.js
+++ src/pages/CustomFunction.js
@@ -1,87 +1,87 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 export function formatDate() {
-    const current = new Date();
-    const months = {
-        0: 'January',
-        1: 'February',
-        2: 'March',
-        3: 'April',
-        4: 'May',
-        5: 'June',
-        6: 'July',
-        7: 'August',
-        8: 'September',
-        9: 'October',
-        10: 'November',
-        11: 'December',
-    }
-    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    const d = current;
-    const year = d.getFullYear()
-    const date = d.getDate()
-    const monthIndex = d.getMonth()
-    const monthName = months[d.getMonth()]
-    const dayName = days[d.getDay()] // Thu
-    const formatted = `${dayName}, ${date} ${monthName} ${year}`
-    return formatted.toString();
+  const current = new Date();
+  const months = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December",
+  };
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const d = current;
+  const year = d.getFullYear();
+  const date = d.getDate();
+  const monthIndex = d.getMonth();
+  const monthName = months[d.getMonth()];
+  const dayName = days[d.getDay()]; // Thu
+  const formatted = `${dayName}, ${date} ${monthName} ${year}`;
+  return formatted.toString();
 }
 
-export function onPageLoad(){
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    document.getElementById('loadercont').style.display = "block";
-    document.getElementById('content').style.display = "none";
-    setTimeout(() => {
-        document.getElementById('loadercont').style.display = "none";
-        document.getElementById('content').style.display = "block";
-        CloseSidenav();
-      }, 1000);
-};
-
-export function ContentHeader(pagetitle,sidenav){
-    document.getElementById("content-heading").innerHTML = pagetitle;
-    
-    const sidenavitem = document.querySelectorAll('.sidenav a');
-    sidenavitem.forEach(sidenavitem => {
-        if(sidenavitem.classList.contains('current')){
-            sidenavitem.classList.remove('current')
-        }
-    });
-    document.querySelector('a.sidenav-'+sidenav).classList.add("current");
-};
-
-export function ThemeSetup(){
-    if(localStorage.getItem("theme")){
-        document.body.classList = [];
-        document.body.classList.add(localStorage.getItem("theme"));
-    }
-}
-export const cloudstorage = 'https://storage.googleapis.com/mark-john-portfolio.appspot.com';
-
-export function Menutoggled(){
-    if (window.innerWidth <= 600){
-        if (document.getElementById('sideNav').classList.contains('active')) {
-            document.getElementById("sideNav").style.transition = "6.0s ease";
-            document.getElementById("sideNav").style.width = "0px";
-    
-            document.getElementById('sideNav').classList.remove('active');
-        }
-        else {
-            document.getElementById("sideNav").style.transition = "6.0s ease";
-            document.getElementById("sideNav").style.width = "auto";
-    
-            document.getElementById('sideNav').classList.add('active');
-        }
-    }
+export function onPageLoad() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  document.getElementById("loadercont").style.display = "block";
+  document.getElementById("content").style.display = "none";
+  setTimeout(() => {
+    document.getElementById("loadercont").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    CloseSidenav();
+  }, 1000);
 }
 
-export function CloseSidenav(){
-    if (window.innerWidth > 600) {
+export function ContentHeader(pagetitle, sidenav) {
+  document.getElementById("content-heading").innerHTML = pagetitle;
+
+  const sidenavitem = document.querySelectorAll(".sidenav a");
+  sidenavitem.forEach((sidenavitem) => {
+    if (sidenavitem.classList.contains("current")) {
+      sidenavitem.classList.remove("current");
+    }
+  });
+  document.querySelector("a.sidenav-" + sidenav).classList.add("current");
+}
+
+export function ThemeSetup() {
+  if (localStorage.getItem("theme")) {
+    document.body.classList = [];
+    document.body.classList.add(localStorage.getItem("theme"));
+  }
+}
+
+export const cloudstorage =
+  "https://storage.googleapis.com/mark-john-portfolio.appspot.com";
+
+export function Menutoggled() {
+  if (window.innerWidth <= 600) {
+    if (document.getElementById("sideNav").classList.contains("active")) {
+      document.getElementById("sideNav").style.transition = "6.0s ease";
+      document.getElementById("sideNav").style.width = "0px";
+
+      document.getElementById("sideNav").classList.remove("active");
+    } else {
+      document.getElementById("sideNav").style.transition = "6.0s ease";
       document.getElementById("sideNav").style.width = "auto";
+
       document.getElementById("sideNav").classList.add("active");
     }
-    else {
-        document.getElementById("sideNav").style.width = "0px";     
-        document.getElementById("sideNav").classList.remove('active');
-    }
+  }
+}
+
+export function CloseSidenav() {
+  if (window.innerWidth > 600) {
+    document.getElementById("sideNav").style.width = "auto";
+    document.getElementById("sideNav").classList.add("active");
+  } else {
+    document.getElementById("sideNav").style.width = "0px";
+    document.getElementById("sideNav").classList.remove("active");
+  }
 }
